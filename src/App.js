fc568c975f60ccde297bf7ff64f2a4e054d797e8
Carlos Valdez <carlosvaldez@Carloss-MacBook-Pro.local>
2020-06-05 10:51:54 -0700

    add in baseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <meta></meta>
+     <Header>
+       <div className="logo" />
+       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+         <Menu.Item key="1">Home</Menu.Item>
+         <Menu.Item key="2">About</Menu.Item>
+         <Menu.Item key="3">Resources</Menu.Item>
+       </Menu>
+     </Header>
+    <Layout>
+      <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <h1>Send email to your elected representatives quickly.</h1>.
+        {/* <div className="site-layout-content">Content</div> */}
+        <Button type="primary" style={{ textAlign: 'right'}}> Get Started</Button>
+      </Content>
+      <Sider style={{ textAlign: 'center' }}>
+        <Button> Get Started</Button>
+      </Sider>
+    </Layout>
+
+     <Footer style={{ textAlign: 'center' }}>Footer</Footer>
+   </Layout>
   );
 }
 
