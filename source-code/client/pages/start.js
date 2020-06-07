be9259c7b777809f89d820a463ca6f969d16672a
Carlos Valdez <carlosvaldez@Carloss-MacBook-Pro.local>
2020-06-07 13:06:28 -0700

    add in start

---
+++ source-code/client/pages/start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/App';
+import Form from '../src/form'
+function Start() {
+    return(
+      <Form></Form>
+    );
+  }
+
+export default Start
