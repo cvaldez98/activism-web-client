0c69f6f979a5ea3618e6135bdfeb449b4ff268e4
Carlos Valdez <carlosvaldez@Carloss-MacBook-Pro.local>
2020-06-09 10:24:45 -0700

    add in credits

--- client/src/Credits.js
+++ client/src/Credits.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { Card, Col, Row } from 'antd';
-
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+import { LinkedinOutlined, GithubOutlined } from '@ant-design/icons'
 
 function Credits() {
     return (
@@ -16,36 +8,43 @@ function Credits() {
         <Row gutter={16} style={{marginBottom: 16}}>
           <Col span={8}>
             <Card title="Alan Ton" bordered={false}>
-                Links
-            </Card>
+                <a href='https://github.com/alandgton/' target='_blank'><GithubOutlined id='icon'/></a>
+                <a href='https://www.linkedin.com/in/alanton/' target='_blank'><LinkedinOutlined id='icon'/></a>
+              </Card>
           </Col>
           <Col span={8}>
             <Card title="Abdallah Abuhashem" bordered={false}>
-                Links
+                <a href='https://github.com/AbdallahAbuHashem' target='_blank'><GithubOutlined id='icon'/></a>
+                <a href='https://www.linkedin.com/in/aabuhash/' target='_blank'><LinkedinOutlined id='icon'/></a>
             </Card>
           </Col>
           <Col span={8}>
             <Card title="Carlos Valdez" bordered={false}>
-                Links
+                <a href='https://github.com/cvaldez98' target='_blank'><GithubOutlined id='icon'/></a>
+                <a href='https://www.linkedin.com/in/carlosvr98/' target='_blank'><LinkedinOutlined id='icon'/></a>
             </Card>
           </Col>
         </Row>
         <Row gutter={16}>
           <Col span={8}>
             <Card title="Uma Krishnan" bordered={false}>
-                Links
+              <div>
+                <a href='' target='_blank'><GithubOutlined id='icon'/></a>
+                <a href='https://www.linkedin.com/in/uma-krishnan/' target='_blank'><LinkedinOutlined id='icon'/></a>
+              </div>
             </Card>
           </Col>
           <Col span={8}>
+            <Card title="Nicolas Renteria" bordered={false}>
+                <a href='https://github.com/nrenteria' target='_blank'><GithubOutlined id='icon'/></a>
+                <a href='https://www.linkedin.com/in/nicolasrenteria/' target='_blank'><LinkedinOutlined id='icon'/></a>
+            </Card>
+          </Col>
+          {/* <Col span={8}>
             <Card title="Card title" bordered={false}>
               Card content
             </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
+          </Col> */}
         </Row>
       </div>
     );
